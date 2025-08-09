6a0021687b799f2e8166f6b40aba00fa0bfe5d8f
Diatoufall <diatoufall278@gmail.com>
2025-08-09 00:14:38 -0400

    Setup model relationships

--- models/index.js
+++ models/index.js
@@ -1,11 +1,31 @@
 const { sequelize } = require('../config/database');
 const { DataTypes } = require('sequelize');
 
-// Chargement du seul modèle disponible
 const User = require('./User')(sequelize, DataTypes);
+const Menu = require('./Menu')(sequelize, DataTypes);
+const Order = require('./Order')(sequelize, DataTypes);
+const Review = require('./Review')(sequelize, DataTypes);
 
-// Export (sans relations car seul le modèle User existe)
+// Relations
+User.hasMany(Menu);
+Menu.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+User.hasMany(Review);
+Review.belongsTo(User);
+
+Menu.hasMany(Review);
+Review.belongsTo(Menu);
+
+Menu.hasMany(Order);
+Order.belongsTo(Menu);
+
 module.exports = {
   sequelize,
-  User
+  User,
+  Menu,
+  Order,
+  Review
 };
