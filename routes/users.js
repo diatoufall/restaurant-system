3240f96e8472cc0a6bc1db8445ee45b562a35914
Diatoufall <diatoufall278@gmail.com>
2025-08-08 23:50:31 -0400

    Add user routes

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+// routes/users.js
+const express = require('express');
+const router = express.Router();
+const UsersController = require('../controllers/UsersController');
+
+router.post('/register', UsersController.register);
+router.post('/login', UsersController.login);
+
+module.exports = router;
